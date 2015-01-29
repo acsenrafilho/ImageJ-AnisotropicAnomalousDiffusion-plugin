7e81233f00f1b9edaf4223ced0705230e331d3c3
Antonio Carlos da Silva Senra Filho <acsenrafilho@gmail.com>
2015-01-29 18:20:51 -0200

    Created new class plugin Isotropic Anomalous DIffusion. Not implemented yet.

---
+++ netbeans-project/src/Anomalous_Diffusion_Filters/Isotropic_Anomalous_Diffusion_2D_Filter.java
@@ -0,0 +1,22 @@
+package Anomalous_Diffusion_Filters;
+
+import ij.plugin.PlugIn;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author antonio
+ */
+public class Isotropic_Anomalous_Diffusion_2D_Filter implements PlugIn{
+
+    @Override
+    public void run(String string) {
+        
+    }
+    
+}
