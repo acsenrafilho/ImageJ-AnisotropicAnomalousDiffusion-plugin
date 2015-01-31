bb22fe1df0912bc275b6ce58c6bf7b6be1eb9f79
Antonio Carlos da Silva Senra Filho <acsenrafilho@gmail.com>
2015-01-31 17:59:56 -0200

    IAD-2D filter completely concluded. User interface with default values checker.

--- netbeans-project/src/Anomalous_Diffusion_Filters/Isotropic_Anomalous_Diffusion_2D_Filter.java
+++ netbeans-project/src/Anomalous_Diffusion_Filters/Isotropic_Anomalous_Diffusion_2D_Filter.java
@@ -1,5 +1,11 @@
 package Anomalous_Diffusion_Filters;
 
+import classes.Diffusion;
+import classes.ImageAccess;
+import ij.IJ;
+import ij.ImagePlus;
+import ij.WindowManager;
+import ij.gui.GenericDialog;
 import ij.plugin.PlugIn;
 
 /*
@@ -7,16 +13,90 @@ import ij.plugin.PlugIn;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author antonio
  */
-public class Isotropic_Anomalous_Diffusion_2D_Filter implements PlugIn{
+public class Isotropic_Anomalous_Diffusion_2D_Filter implements PlugIn {
+//TODO Aprimorar plugin para rodar com imagestack, mesmo com processamento 2D. Rever filtro IAD, em questao do uso do parametro delta.
+    
+    final int imageDimension = 2;
+
+    String[] choiceItemsEdgeFunction = {"Exponential", "Fractional", "Partial"};
+    Diffusion diff = new Diffusion();
+    final double delta_limit = 1 / Math.pow(2, imageDimension + 1);
+    final double default_q = 1.0, default_Dq = 1.0, default_delta = delta_limit;
+    final int default_iterations = 5;
+    double q, Dq, delta;
+    int iterations;
 
     @Override
     public void run(String string) {
-        
+        //Get the image opened in the current ImageJ scene.
+        ImagePlus img = WindowManager.getCurrentImage();
+
+        if (img == null) {
+            IJ.showMessage("No image open", "There is no image open\nPlease, open an image first");
+            return;
+        }
+
+        //Test if the image open is not an RGB image.
+        if (img.getType() == ImagePlus.COLOR_RGB) {
+            IJ.showMessage("Not gray scale image", "The image open is not gray scale\nPlease, convert the image to 8-bits, 16-bits or 32 bits gray scale.");
+            return;
+        }
+
+        GenericDialog gd = new GenericDialog("Isotropic Anomalous Diffusion - 2D");
+        gd.setOKLabel("Run");
+
+        gd.addNumericField("Anomalous parameter (Q)", default_q, 4);
+        gd.addNumericField("Generalized Diffusion Coeff.", default_Dq, 4);
+        gd.addNumericField("Time step", default_delta, 4);
+        gd.addNumericField("Number of iterations", default_iterations, 1);
+        gd.addMessage("If any values were set wrong,\nthe default values will be used.");
+
+        //gd.addCheckbox("Use automatic condutance estimator function", false);
+        gd.showDialog();
+
+        //Close diaglog if the user click on the Cancel button.
+        if (gd.wasCanceled()) {
+            return;
+        }
+
+        if (gd.wasOKed()) {
+            q = gd.getNextNumber();
+            Dq = gd.getNextNumber();
+            delta = gd.getNextNumber();
+            iterations = (int) gd.getNextNumber();
+            //Check the parameters values set up from the user. If anything is wrong, it is used the default values.
+            checkParametersValues();
+
+            diff.setQ(q);
+            diff.setD(Dq);
+            diff.setDelta(delta);
+            diff.setNumInteration(iterations);
+            diff.AnomalousIsoDiff(new ImageAccess(img.getProcessor()), Dq).show("IAD-Q" + diff.getQ() + "numInt" + diff.getNumInteration() + "timeStep" + diff.getDelta() + "Dq" + diff.getD());
+        }
+
     }
-    
+
+    private void checkParametersValues() {
+        //Test for the anomalous parameter (Q)
+        if (q < 0d || q > 2.0) {
+            q = default_q;
+        }
+        //Test for the generalized diffusion coefficient parameter (Dq)
+        if (Dq < 0d || Dq >1.0) {
+            Dq = default_Dq;
+        }
+        //Test for the time step parameter (delta)
+        if (delta > delta_limit) {
+            delta = default_delta;
+        }
+        //Test for the number of iterations parameter (t)
+        if (iterations < 0) {
+            iterations = default_iterations;
+        }
+    }
+
 }
